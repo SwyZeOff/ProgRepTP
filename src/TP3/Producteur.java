de4ee6ca70a3fd7482551103db022fa042c0907f
SwyZe <alexandre.martin@swyze.fr>
2020-04-24 10:03:39 +0200

    Addition of a second producer

--- src/TP3/Producteur.java
+++ src/TP3/Producteur.java
@@ -11,8 +11,7 @@ public class Producteur extends Thread {
 	
 	public void run() {
 		for(int i=0; i<20; i++) {
-			message = "Ceci est le message " + i + "\n";
-			bal.setMail(message);
+			bal.setMail(" ["+i+"]"+message+"\n");
 		}
 	}
 }
--- src/TP3/main.java
+++ src/TP3/main.java
@@ -5,9 +5,11 @@ public class main {
 	public static void main(String[] args) {
 		
 		BAL baba = new BAL();
-		Producteur produ = new Producteur(baba, "titi");
+		Producteur produ = new Producteur(baba, "Baguette");
+		Producteur produ2 = new Producteur(baba, "Pain au chocolat");
+
 		Consommateur conso = new Consommateur(baba, "");
-		
+		produ2.start();
 		produ.start();
 		conso.start();
 		
